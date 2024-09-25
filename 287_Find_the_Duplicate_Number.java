f4c1fb82f9094325ced0ec736b01ae92f903facb
Khuat Quang Hung <hungsct1702@gmail.com>
2024-09-25 22:16:34 +0700

    update

--- 287_Find_the_Duplicate_Number.java
+++ 287_Find_the_Duplicate_Number.java
@@ -7,17 +7,17 @@ class Solution {
         int fast = nums[0];
         // slow = 1   fast = 1 
         // Phase 1: Finding the intersection point of the two runners.
-        do {  // 1 3 4 2 2
+        do {  // 1  3 4 2 2
             slow = nums[slow]; // slow = 2
-            fast = nums[nums[fast]];    // fast = 2 
+            fast = nums[nums[fast]];    // fast = 2
         } while (slow != fast);
 
         // Phase 2: Finding the entrance to the cycle.
-        
+
         slow = nums[0];   // slow = 1 
         while (slow != fast) {
             slow = nums[slow];  // slow = 2
-            fast = nums[fast];  // fast = 2 
+            fast = nums[fast];  // fast = 2
         }
 
         return slow;
